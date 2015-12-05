625feb8082611ba93357fbdd284cb17b4d7cf0a0
Daniel Cunsolo <daniel.cunsolo@bellmedia.ca>
2015-12-05 17:22:20 -0500

    add search directive to filter post list

--- wp-content/themes/angularwordpress/functions.php
+++ wp-content/themes/angularwordpress/functions.php
@@ -3,7 +3,8 @@
 function add_scripts(){
   wp_register_script('angularjs', get_stylesheet_directory_uri() . '/node_modules/angular/angular.min.js');
   wp_register_script('angularjs-route', get_stylesheet_directory_uri() . '/node_modules/angular-route/angular-route.min.js');
-  wp_enqueue_script('my-scripts', get_stylesheet_directory_uri(). '/js/scripts.js', array('angularjs', 'angularjs-route') );
+  wp_register_script('angularjs-sanitize', get_stylesheet_directory_uri() . '/node_modules/angular-sanitize/angular-sanitize.min.js');
+  wp_enqueue_script('my-scripts', get_stylesheet_directory_uri(). '/js/scripts.js', array('angularjs', 'angularjs-route', 'angularjs-sanitize') );
   wp_localize_script('my-scripts', 'myLocalized', array('partials' => trailingslashit( get_template_directory_uri() ) . 'partials/', 'nonce' => wp_create_nonce( 'wp_rest' ) ) );
 }
 add_action('wp_enqueue_scripts', 'add_scripts' );
--- wp-content/themes/angularwordpress/js/scripts.js
+++ wp-content/themes/angularwordpress/js/scripts.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngSanitize']);
 
 myApp.config(function($routeProvider, $locationProvider){
   $locationProvider.html5Mode(true);
@@ -15,6 +15,7 @@ myApp.config(function($routeProvider, $locationProvider){
 
 })
 
+//Main ctrl to display post list
 myApp.controller('Main', function($scope, $http, $routeParams){
   $http.get('wp-json/posts/').success(function(result){
     $scope.posts = result;
@@ -22,8 +23,27 @@ myApp.controller('Main', function($scope, $http, $routeParams){
   //console.log('main file loaded');
 })
 
+//Content ctrl to handle the display of a single posts content
 myApp.controller('Content', function($scope, $http, $routeParams){
   $http.get('wp-json/posts/' + $routeParams.ID).success(function(result){
     $scope.post = result;
   });
+})
+
+//Simple search to filter posts list
+// -pass search input to filter, set posts to result
+myApp.directive('searchForm', function(){
+  return{
+    restrict: 'EAC',
+    template: 'Search post: <input type="text" name="s" ng-model="filter.s" ng-change="search()" >',
+    controller: function($scope, $http){
+      $scope.filter = { s: '' };
+      $scope.search = function(){
+        $http.get('wp-json/posts/?filter[s]=' + $scope.filter.s)
+          .success(function(result){
+            $scope.posts = result;
+          });
+      }
+    }
+  };
 });
